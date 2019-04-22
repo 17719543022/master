d20095604932b1781e3aeec67c097b10933e5810
17719543022 <xin_nian13@163.com>
2019-04-22 16:04:26 +0800

    不跟踪FaceSDKTest - 副本

--- FaceSDKTest - 副本/.svn/pristine/c3/c35840af4037cf16ab4d5e072776c8bd07987839.svn-base
+++
@@ -1,5 +0,0 @@
-#pragma once
-
-#define MINFACESIZE 20
-#define MAXFACESIZE 200
-#define MAXTHREADNUM 10			//最大并发数量
--- FaceSDKTest - 副本/SDKTest/LogHelper.h
+++
@@ -1,8 +0,0 @@
-#pragma once
-
-void initLogFile();
-
-void printMsg(const char* format, ...);
-
-void printErrMsg(const char* format, ...);
-

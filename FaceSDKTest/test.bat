1911e2de9dac984149d997a3e46d38023389339d
17719543022 <xin_nian13@163.com>
2019-05-16 17:27:50 +0800

    基本上所有的需求都简洁地完成了，可以设置单独跑某条某几条测试用例，可以打印测试报告到文档

--- FaceSDKTest/test.bat
+++ FaceSDKTest/test.bat
@@ -1,7 +1,7 @@
 set current_dir=.\Project\FaceSDKTest
 pushd %current_dir%
 path = %path%.\..;.\..\..\Export\face\build_32\bin;.\..\..\Export\opencv\build\x86\vc11\bin;.\..\..\Export\Pre-built.2\dll\x86;
-.\Debug\FaceSDKTest.exe
+.\Debug\FaceSDKTest.exe --gtest_filter=ftInterface.getAFacePcaFeatureAndSave>.\..\..\report.txt
 popd
 
 pause
